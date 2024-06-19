bb80fef80570519063ebc73480c69ce99b96dd68
rajesh <rajeshveeranki478@gmail.com>
2024-06-19 22:47:10 +0530

    jenkins

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 
-
-AMI=ami-0b4f379183e5706b9 #this keeps on changing 
+AMI=ami-0b4f379183e5706b9 #this keeps on changing
 SG_ID=sg-03878c39dc8a089a1 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z0251190AS2ECJ9G31JH # replace your zone ID
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-02cd2457826a3b260 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
